5df3bb2145963a2ded537a62295a65b98c4b732e
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2023-12-24 14:50:32 +0530

    Initial commit

---
+++ DSA Exercises/Recursion_Exercise1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int f(int n){
+    static int i = 1;
+    if(n>=5)return n;
+    n=n+i;
+    i++;
+    return f(n);
+}
+int main(){
+    cout << f(1);
+}
+
