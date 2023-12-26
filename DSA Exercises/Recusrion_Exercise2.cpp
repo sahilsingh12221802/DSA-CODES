f6ee484f3aeabb793b88aefb248056318d0c807c
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2023-12-26 16:32:11 +0530

    Add files via upload

---
+++ DSA Exercises/Recusrion_Exercise2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void foo(int n, int sum){
+    int k = 0, j = 0;
+    if(n == 0)return;
+    k = n % 10;
+    j = n/10;
+    sum=sum+k;
+    foo(j,sum);
+    cout << k <<",";
+}
+int main(){
+    int a = 2048, sum = 0;
+    foo(a,sum);
+    cout << sum;
+}
