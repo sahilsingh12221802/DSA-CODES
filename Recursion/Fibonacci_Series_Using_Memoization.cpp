c72dfe8888fd8f6154a52301a04ba837d30ff72c
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-01-03 16:52:47 +0530

    Add files via upload

---
+++ Recursion/Fibonacci_Series_Using_Memoization.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int F[10];
+int fib(int n){
+    if(n <= 1){
+        F[n] = n;
+        return n;
+    }else{
+        if(F[n-2]==-1){
+            F[n-2] = fib(n-2);
+        }
+        if(F[n-1]==-1){
+            F[n-1]=fib(n-1);
+        }
+        return F[n-2]+F[n-1];
+    }
+}
+int main(){
+    int r,i;
+    for(i = 0; i<10; i++){
+        F[i]=-1;
+    }
+    r=fib(10);
+    cout << r;
+}
