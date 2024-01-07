88d0720de96ab9d820db0a0b27732b4cdbd7f975
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-01-07 16:11:49 +0530

    Add files via upload

---
+++ DSA Exercises/Recusrion_Exercise4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int fun(int n){
+    int x = 1, k;
+    if(n==1) return x;
+    for(k=1;k<n;k++){
+        x = x + fun(k) * fun(n-k);
+    }
+    return x;
+}
+int main(){
+    cout << fun(5);
+}
