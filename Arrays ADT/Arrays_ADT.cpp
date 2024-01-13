eca23b627e9b9f02f3757bdde5a22198dbf2b4fb
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-01-13 14:35:16 +0530

    Create Arrays_ADT.cpp

---
+++ Arrays ADT/Arrays_ADT.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct Array{
+    int *A;
+    int size;
+    int length;
+};
+void display(struct Array arr){
+    int i;
+    cout << "The elements of an array: ";
+    for(i=0;i<arr.length;i++){
+        cout<<arr.A[i]<<" ";
+    }
+
+}
+
+int main(){
+    Array arr;
+    int n,i;
+    cout << "Enter the size of the array: ";
+    cin >> arr.size;
+    arr.A = new int[arr.size];
+    arr.length = 0;
+    cout << "Enter the number of elements that should be present in the array: ";
+    cin>>n;
+    cout << "Enter the elements for an array: ";
+    for(i=0;i<n;i++){
+        cin>>arr.A[i];
+    }
+    arr.length = n;
+
+    display(arr);
+
+}
