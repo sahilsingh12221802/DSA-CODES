6f4941eb0ef3a05a9c3be4fbd75e5ccc93900b5b
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-01-24 19:02:05 +0530

    Add files via upload

---
+++ Arrays ADT/Set_Operation_On_Array.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+using namespace std;
+
+struct Array{
+    int *A;
+    int size;
+    int length;
+};
+void display(struct Array arr){
+    int i;
+    for(i=0;i<arr.length;i++){
+        cout<<arr.A[i]<<" ";
+    }
+
+}
+
+struct Array* Merge(struct Array *arr1, struct Array *arr2,struct Array *arr3){
+    int i,j,k;
+    i=j=k=0;
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i]<arr2->A[j]){
+            arr3->A[k++]=arr1->A[i++];
+        }else{
+            arr3->A[k++]=arr2->A[j++];
+        }
+    }
+    for(;i<arr1->length;i++){
+        arr3->A[k++] = arr1->A[i];
+    }
+    for(;j<arr2->length;j++){
+        arr3->A[k++] = arr2->A[j];
+    }
+    arr3->length = arr1->length + arr2->length;
+    return arr3;
+}
+
+struct Array* Union(struct Array *arr1, struct Array *arr2,struct Array *arr3){
+    int i,j,k;
+    i=j=k=0;
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i]<arr2->A[j]){
+            arr3->A[k++]=arr1->A[i++];
+        }else if(arr2->A[j]<arr1->A[i]){
+            arr3->A[k++]=arr2->A[j++];
+        }else{
+            arr3->A[k++]=arr1->A[i++];
+            j++;
+        }
+    }
+    for(;i<arr1->length;i++){
+        arr3->A[k++] = arr1->A[i];
+    }
+    for(;j<arr2->length;j++){
+        arr3->A[k++] = arr2->A[j];
+    }
+    arr3->length = k;
+    return arr3;
+}
+struct Array* Intersection(struct Array *arr1, struct Array *arr2,struct Array *arr3){
+    int i,j,k;
+    i=j=k=0;
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i]<arr2->A[j]){
+            i++;
+        }else if(arr2->A[j]<arr1->A[i]){
+            j++;
+        }else if(arr1->A[i]==arr2->A[j]){
+            arr3->A[k++]=arr1->A[i++];
+            j++;
+        }
+    }
+    arr3->length = k;
+    return arr3;
+}
+
+struct Array* Difference(struct Array *arr1, struct Array *arr2,struct Array *arr3){
+    int i,j,k;
+    i=j=k=0;
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i]<arr2->A[j]){
+            arr3->A[k++]=arr1->A[i++];
+        }else if(arr2->A[j]<arr1->A[i]){
+            j++;
+        }else{
+            i++;
+            j++;
+        }
+    }
+    for(;i<arr1->length;i++){
+        arr3->A[k++] = arr1->A[i];
+    }
+    arr3->length = k;
+    return arr3;
+}
+
+int main(){
+    Array arr1,arr2,arr3;
+    int n,i;
+    cout << "Enter the size of the array 1: ";
+    cin >> arr1.size;
+    arr1.A = new int[arr1.size];
+    arr1.length = 0;
+    cout << "Enter the number of elements that should be present in the array 1: ";
+    cin>>n;
+    cout << "Enter the elements for an array 1: ";
+    for(i=0;i<n;i++){
+        cin>>arr1.A[i];
+    }
+    arr1.length = n;
+    cout << "Elements of array 1 are: ";
+    display(arr1);
+
+    cout << "\nEnter the size of the array 2: ";
+    cin >> arr2.size;
+    arr2.A = new int[arr2.size];
+    arr2.length = 0;
+    cout << "Enter the number of elements that should be present in the array 2: ";
+    cin>>n;
+    cout << "Enter the elements for an array 2: ";
+    for(i=0;i<n;i++){
+        cin>>arr2.A[i];
+    }
+    arr2.length = n;
+    cout << "Elements of array 2 are: ";
+    display(arr2);
+    cout << endl;
+    arr3.size = arr1.size + arr2.size;
+    arr3.A = new int[arr3.size];
+
+    Merge(&arr1,&arr2,&arr3);
+    cout << "Elements of array after merging are: ";
+    display(arr3);
+
+    Union(&arr1,&arr2,&arr3);
+    cout << "\nElements of array after union operation are: ";
+    display(arr3);
+
+    Intersection(&arr1,&arr2,&arr3);
+    cout << "\nElements of array after Intersection operation are: ";
+    display(arr3);
+
+    Difference(&arr1,&arr2,&arr3);
+    cout << "\nElements of array after Difference of (A-B) is: ";
+    display(arr3);
+}
